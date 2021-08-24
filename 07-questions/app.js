67090f8c547350143a7c210641a1355a97aad5dc
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-08-24 15:52:07 +0100

    hide-text

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -2,12 +2,15 @@
 // traversing the dom
 
 questionBtns = document.querySelectorAll('.question-btn');
+const questionText = document.querySelectorAll('.question');
+
 questionBtns.forEach(item => {
 	item.addEventListener('click', (e) => {
+		hideText()
 		elem = e.currentTarget.parentElement.parentElement;
-
+		//console.log(elem)
 		//One liner code 
-		elem.classList.toggle('show-text')
+		//elem.classList.toggle('show-text')
 		//traditional way i can do it
 		if (elem.classList.contains('show-text')) {
 			elem.classList.remove('show-text')
@@ -15,5 +18,13 @@ questionBtns.forEach(item => {
 			elem.classList.add('show-text')
 		}
 	})
+
 })
 
+function hideText() {
+	questionText.forEach(newItem => {
+		if (newItem.classList.contains('show-text')) {
+			newItem.classList.remove('show-text')
+		}
+	})
+}
