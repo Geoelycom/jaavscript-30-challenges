844963d7da6d5304e28f364130c5795f0e06add0
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-09-01 22:59:43 +0100

    add html-file and css

---
+++ 15-grocery-bud/app.js
@@ -0,0 +1,21 @@
+ // ****** SELECT ITEMS **********
+
+const alert = document.querySelector('.alert');
+const submitBtn = document.querySelector('.submit-btn');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn')
+
+// edit option
+let editElement;
+let editFlag = false;
+let editId = '';
+// ****** EVENT LISTENERS **********
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
